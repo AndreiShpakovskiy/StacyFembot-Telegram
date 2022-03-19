416fe14d9a836971092f9d6cb497589c849fed8d
AndreiShpakovskiy <shpakovskiyandrei@gmail.com>
2022-03-19 20:38:04 +0300

    Add Java-like Main class

--- src/main/java/com/bot/stacy/Main.kt
+++ src/main/java/com/bot/stacy/Main.kt
@@ -3,6 +3,11 @@ package com.bot.stacy
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
-fun main() {
-    TelegramBotsApi(DefaultBotSession::class.java).registerBot(StacyFembot())
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            TelegramBotsApi(DefaultBotSession::class.java).registerBot(StacyFembot())
+        }
+    }
 }
